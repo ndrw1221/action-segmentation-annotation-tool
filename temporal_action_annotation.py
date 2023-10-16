2b40cc306d51a1b5eaad129308e365c2743aa733
Andrew Hsieh <andrew891221@gmail.com>
2023-10-16 17:25:55 +0800

    Added keyboard control functionality

--- temporal_action_annotation.py
+++ temporal_action_annotation.py
@@ -1,7 +1,9 @@
 import sys
 import os
+import typing
+from PyQt5 import QtCore, QtGui
 import cv2
-from PyQt5.QtCore import QTimer
+from PyQt5.QtCore import QTimer, Qt
 from PyQt5.QtGui import QImage, QPixmap, QFont
 from PyQt5.QtWidgets import (
     QApplication,
@@ -18,6 +20,7 @@ from PyQt5.QtWidgets import (
     QListWidget,
     QGroupBox,
     QListWidgetItem,
+    QAction,
 )
 
 
@@ -31,6 +34,8 @@ class VideoPlayerApp(QMainWindow):
         self.fps = 30
         self.actions = {}
         self.video_name = ""
+
+        self.keyPress()
 
         self.initUI()
 
@@ -50,43 +55,53 @@ class VideoPlayerApp(QMainWindow):
         self.delete_action_button = QPushButton("Delete", self)
         self.delete_action_button.setEnabled(False)
         self.delete_action_button.clicked.connect(self.delete_action)
+        self.delete_action_button.setFocusPolicy(Qt.ClickFocus)
 
         self.frame_label = QLabel("Frame: 0", self)
 
         self.slider = QSlider(1, self)
         self.slider.setEnabled(False)
         self.slider.valueChanged.connect(self.onSliderChange)
+        self.slider.setFocusPolicy(Qt.ClickFocus)
 
-        self.load_button = QPushButton("Load Video", self)
+        self.load_button = QPushButton("Load Video (L)", self)
         self.load_button.clicked.connect(self.loadVideo)
+        self.load_button.setFocusPolicy(Qt.ClickFocus)
 
-        self.play_button = QPushButton("Play", self)
+        self.play_button = QPushButton("Play (Space)", self)
         self.play_button.setEnabled(False)
         self.play_button.clicked.connect(self.playVideo)
+        self.play_button.setFocusPolicy(Qt.ClickFocus)
 
-        self.forward_button = QPushButton("Forward", self)
+        self.forward_button = QPushButton("Forward (→)", self)
         self.forward_button.setEnabled(False)
         self.forward_button.clicked.connect(self.forwardFrame)
+        self.forward_button.setFocusPolicy(Qt.ClickFocus)
 
-        self.backward_button = QPushButton("Backward", self)
+        self.backward_button = QPushButton("Backward (←)", self)
         self.backward_button.setEnabled(False)
         self.backward_button.clicked.connect(self.backwardFrame)
+        self.backward_button.setFocusPolicy(Qt.ClickFocus)
 
-        self.add_action_button = QPushButton("Add Action", self)
-        # self.add_action_button.setEnabled(False)
+        self.add_action_button = QPushButton("Add Action (A)", self)
+        self.add_action_button.setEnabled(False)
         self.add_action_button.clicked.connect(self.addTemporalAction)
+        self.add_action_button.setFocusPolicy(Qt.ClickFocus)
 
-        self.action_start_button = QPushButton("Action Start", self)
+        self.action_start_button = QPushButton("Action Start (F)", self)
         self.action_start_button.setEnabled(False)
         self.action_start_button.clicked.connect(self.temporalActionStart)
+        self.action_start_button.setFocusPolicy(Qt.ClickFocus)
 
-        self.action_end_button = QPushButton("Action End", self)
+        self.action_end_button = QPushButton("Action End (D)", self)
         self.action_end_button.setEnabled(False)
         self.action_end_button.clicked.connect(self.temporalActionEnd)
+        self.action_end_button.setFocusPolicy(Qt.ClickFocus)
 
-        self.output_annotation_button = QPushButton("Output Annotation", self)
+        self.output_annotation_button = QPushButton("Output Annotation (O)", self)
         self.output_annotation_button.setEnabled(False)
         self.output_annotation_button.clicked.connect(self.outputAnnotation)
+        self.output_annotation_button.setFocusPolicy(Qt.ClickFocus)
 
         group_box = QGroupBox("Actions")
         group_box.setFont(font_14)
@@ -283,6 +298,8 @@ class VideoPlayerApp(QMainWindow):
             for line in output_format:
                 file.write(line + "\n")
 
+        dialog = OuputAnnotationDialog()
+        dialog.exec_()
         print(f"{file_name}.txt created successfully.")
 
     def delete_action(self):
@@ -293,6 +310,52 @@ class VideoPlayerApp(QMainWindow):
             del self.actions[selected_action.text()]
             self.updateButtonState()
             print(f"'{selected_action.text()}' is deleted.")
+
+    def keyPress(self):
+        # L or l for loading video
+        self.loadVideoAction = QAction("Load video", self)
+        self.loadVideoAction.setShortcut("L")
+        self.loadVideoAction.triggered.connect(lambda: self.load_button.click())
+        self.addAction(self.loadVideoAction)
+        # Spacebar to play and pause video
+        self.playVideoAction = QAction("Play video", self)
+        self.playVideoAction.setShortcut("Space")
+        self.playVideoAction.triggered.connect(lambda: self.play_button.click())
+        self.addAction(self.playVideoAction)
+        # Left arrow to backward one frame
+        self.backwardAction = QAction("Previous frame", self)
+        self.backwardAction.setShortcut("Left")
+        self.backwardAction.triggered.connect(lambda: self.backward_button.click())
+        self.addAction(self.backwardAction)
+        # Rigth arrow to forward one frame
+        self.forwardAction = QAction("Next frame", self)
+        self.forwardAction.setShortcut("Right")
+        self.forwardAction.triggered.connect(lambda: self.forward_button.click())
+        self.addAction(self.forwardAction)
+        # A or a to add temporal acion
+        self.addActionAction = QAction("Add action", self)
+        self.addActionAction.setShortcut("A")
+        self.addActionAction.triggered.connect(lambda: self.add_action_button.click())
+        self.addAction(self.addActionAction)
+        # F or f to mark action start
+        self.actionStartAction = QAction("Mark action start", self)
+        self.actionStartAction.setShortcut("F")
+        self.actionStartAction.triggered.connect(
+            lambda: self.action_start_button.click()
+        )
+        self.addAction(self.actionStartAction)
+        # D or d to make action end
+        self.actionEndAction = QAction("Mark action end", self)
+        self.actionEndAction.setShortcut("D")
+        self.actionEndAction.triggered.connect(lambda: self.action_end_button.click())
+        self.addAction(self.actionEndAction)
+        # O or o to make action end
+        self.outputAnnotationAction = QAction("Output annotation", self)
+        self.outputAnnotationAction.setShortcut("O")
+        self.outputAnnotationAction.triggered.connect(
+            lambda: self.output_annotation_button.click()
+        )
+        self.addAction(self.outputAnnotationAction)
 
 
 class ActionNameInputDialog(QDialog):
@@ -315,6 +378,30 @@ class ActionNameInputDialog(QDialog):
 
         Vlayout = QVBoxLayout()
         Vlayout.addWidget(self.action_input)
+        Vlayout.addWidget(ok_button)
+
+        self.setLayout(Vlayout)
+
+
+class OuputAnnotationDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(" ")
+
+        font = QFont()
+        font.setPointSize(16)
+
+        label = QLabel("Annotation output success.")
+        label.setFont(font)
+
+        ok_button = QPushButton("OK")
+        ok_button.clicked.connect(self.accept)
+
+        Vlayout = QVBoxLayout()
+        Vlayout.addWidget(label)
         Vlayout.addWidget(ok_button)
 
         self.setLayout(Vlayout)
